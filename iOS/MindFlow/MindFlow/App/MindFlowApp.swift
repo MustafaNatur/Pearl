f56739866047061912695118b82f5b2fa0aeea91
Mustikkk <mustikk.natur@mail.ru>
2025-06-22 23:49:28 +0300

    main: project init

---
+++ iOS/MindFlow/MindFlow/App/MindFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindFlowApp.swift
+//  MindFlow
+//
+//  Created by Mustafa on 22.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MindFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
